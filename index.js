63e285bc34a89e5e69cc92214f017dbe45f30dec
Damian Barera <team@platzi.com>
2022-05-18 11:12:01 -0300

    Creada hoja de estilos

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const baseUrl = "https://platzi-avo.vercel.app";
-const appNode = document.querySelector("#app")
+const appNode = document.querySelector("#app");
+
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat("en-EN", {
+        style: "currency",
+        currency: "USD",
+    }).format(price);
+    return newPrice;
+};
+
 window
     .fetch(`${baseUrl}/api/avo`)
     .then((response) => response.json())
@@ -8,17 +17,21 @@ window
         responseJson.data.forEach ((data) => {
             const image = document.createElement("img");
             image.src = `${baseUrl}${data.image}`;
+            
 
             const title = document.createElement("h2");
             title.textContent = data.name;
             title.style.color = "blue";
 
             const price = document.createElement("span");
-            price.textContent = data.price;
+            price.textContent = formatPrice(data.price);
+            price.style.color = "grey";
             
             const container = document.createElement("div");
             container.append(image, title, price);
             container.style.border = "2px solid black";
+            container.style.width = "30%";
+            
             allItems.push(container);
         });
         appNode.append(...allItems);
